5056886f7469d4be57c9d321cc38bb671a4034a1
Harold Gomez <p.ice.taa@gmail.com>
2023-10-24 18:32:00 -0500

    Se reestructura el codigo inicial, y se comienza a implementar en conjunto parte a parte el CSS , el HTML y la logica de Js

---
+++ Js/main.js
@@ -0,0 +1,13 @@
+//Barra de busqueda
+const searchButton=document.getElementById('search-button');
+const pokeNameInput= document.getElementById('poke-name')
+
+searchButton.addEventListener('click',()=>{
+    const pokemonName=pokeNameInput.value.trim();
+    //Pendiente de la logica del boton de busqueda.
+    //solicitud a la api etc.
+
+
+
+
+})
