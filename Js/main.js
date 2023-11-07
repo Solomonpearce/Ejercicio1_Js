9177b6b6a5da40749eb109b657ffe50735aeb2fb
Harold Gomez <p.ice.taa@gmail.com>
2023-11-07 18:41:36 -0500

    Se hace una reestructuracion en como se estaba trabajando, se quita la lista en el HTML y se agrega una logica capaz de organizar el set de movimientos

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,50 @@
 //Barra de busqueda
 const searchButton=document.getElementById('search-button');
-const pokeNameInput= document.getElementById('poke-name')
+const pokeNameInput= document.getElementById('poke-name');
+const pokemonInfo=document.getElementById('pokemon-info');
+const evolveButton=document.getElementById('evolve-button');
+const homeLink=document.getElementById('home-link');
 
-searchButton.addEventListener('click',()=>{
+//funcion para detalles pokemon
+function showPokemonInfo(data){
+    const pokemonNameInfo=document.getElementById('pokemon-name-info');
+    const pokemonImage=document.getElementById('pokemon-image');
+    const descripcionPokemon=document.getElementById('descripcion-pokemon');
+    const movimientosPokemon=document.getElementById('movimientos-pokemon');
+
+    pokemonNameInfo.textContent=data.name;
+    pokemonImage.src=data.sprites.front_default;
+    descripcionPokemon.textContent='Descripción del pokemon: ' //Pendiente por implementacion
+    //logica movimientos  pokemon
+    movimientosPokemon.innerHTML='';
+    data.moves.slice(0,5).forEach((move)=>{
+        const li=document.createElement('li');
+        li.textContent=move.move.name;
+        movimientosPokemon.appendChild(li);
+    });
+
+    pokemonInfo.classList.remove('hidden');
+    //mostrar el boton de evolucion, segun corresponda
+    //evolveButton.classList.remove('hidden'); //Pendiente por implementacion
+
+
+}
+searchButton.addEventListener('click',async ()=>{
+    //Nombre de pokemon ingresado
     const pokemonName=pokeNameInput.value.trim();
-    //Pendiente de la logica del boton de busqueda.
-    //solicitud a la api etc.
+
+    // Solicitud a la API
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        if (!response.ok){
+            throw new Error("No se encontro el pokemon,verifica el nombre");
+        }
 
 
-
-
-})
+        const data=await response.json();
+        showPokemonInfo(data);
+    }catch (error){
+        console.error("Error al cargar los detalles del pokemon",error);
+    }
+});
+        
